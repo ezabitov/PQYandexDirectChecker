c028ef80672f790f154b6ef44d7eddc1459d99fe
ezabitov <e.zabitov@gmail.com>
2017-10-23 00:13:37 +0300

    Update fnCampaignOptions.m

--- fnCampaignOptions.m
+++ fnCampaignOptions.m
@@ -53,7 +53,7 @@ let
     expandDailyBudget = Table.ExpandRecordColumn(expandCampaigns1, "Campaigns", {"Name", "Id", "DailyBudget"}, {"Name", "Id", "DailyBudget"}),
     expandDailyBudget2 = Table.ExpandRecordColumn(expandDailyBudget, "DailyBudget", {"Mode", "Amount"}, {"DailyBudget.Mode", "DailyBudget.Amount"}),
     budgetTypeChange = Table.TransformColumnTypes(expandDailyBudget2,{{"DailyBudget.Amount", Int64.Type}}),
-    budgetToReal = Table.TransformColumns(#"Измененный тип", {{"DailyBudget.Amount", each _ / 1000000, type number}}),
+    budgetToReal = Table.TransformColumns(budgetTypeChange, {{"DailyBudget.Amount", each _ / 1000000, type number}}),
     unpivotDailyBudget = Table.UnpivotOtherColumns(budgetToReal, {"Name", "Id"}, "Option", "Value"),
     append = Table.Combine({expandSettings2, unpivotRelKey, renameItemsToValue, addValueEmail, unpivotStrategy, unpivotDailyBudget}),
     delAnother = Table.SelectColumns(append,{"Value", "Option"}),
